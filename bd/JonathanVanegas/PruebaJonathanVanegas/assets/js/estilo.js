7f1e277d0f5999956e2b27ae8ce7cb99db26a413
Jonathan <jonjova1992@hotmail.com>
2022-03-10 12:22:24 -0600

    Informacion y base de datos

--- bd/JonathanVanegas/PruebaJonathanVanegas/assets/js/estilo.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('#nav-toggler').addEventListener('click', function() {
-
-    document.querySelector('#nav-toggler').classList.toggle('nav-toggle');
-    document.querySelector('.mobnav-bg').classList.toggle('menu-open');
-
-
-});
